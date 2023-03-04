72ac7cfd371eb44a454d36aec18c8199ec8dab8d
hallhassi <hallhassi@gmail.com>
2023-03-04 05:10:09 +0000

    a

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
+require('./database/database').connect()
+const User = require('./model/user')
 const express = require('express')
-
 const app = express()
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("<p>/</p>")
@@ -9,6 +11,18 @@ app.get("/", (req, res) => {
 
 app.get("/hi", (req, res) => {
     res.send("<p>hi</p>")
+})
+
+app.post("/register", async (req, res) => {
+    try {
+        const body = req.body
+        const exists = await User.findOne(body)
+        if (exists){
+            await User.create(body)
+        }        
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 
